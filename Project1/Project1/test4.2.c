5c7c4e402d143becb9cbe3faa93ede7f6a3b2996
super0099 <2979226224@qq.com>
2022-04-06 16:10:22 +0800

    4.6 16:10

--- Project1/Project1/test4.2.c
+++ Project1/Project1/test4.2.c
@@ -16,25 +16,128 @@
 //}
 
 
-int* test() {
-	int a = 10;
-	return &a;
-}
-/*野指针*/
-int main() {
-	//1.随机野指针
-	//int *p;//局部指针变量被初始化为随机值
-	////2.指针溢出
-	//int a[10] = {0};
-	//int* b = &a;
-	////如果不控制i < 10和数组长度相等,那么指针就会溢出
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	*(p + 1) = 1;
-	//}
-	//空指针
-	int* a = test();//因为方法内部的变量a,随着代码执行完成后立刻被系统回收虽然有返回地址,但是这个地址已经不属于&a了,所以无法修改
+//int* test() {
+//	int a = 10;
+//	return &a;
+//}
+///*野指针*/
+//int main() {
+//	//1.随机野指针
+//	//int *p;//局部指针变量被初始化为随机值
+//	////2.指针溢出
+//	//int a[10] = {0};
+//	//int* b = a;
+//	////如果不控制i < 10和数组长度相等,那么指针就会溢出
+//	//for (int i = 0; i < 10; i++)
+//	//{
+//	//	*b++= 1;
+//	//}
+//	//空指针
+//	int* a = test();//因为方法内部的变量a,随着代码执行完成后立刻被系统回收虽然有返回地址,但是这个地址已经不属于&a了,所以无法修改
+//
+//	printf("%d\n", *a);
+//}
 
-	printf("%d\n", *a);
+//int main() {
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int* a = arr;//获取第一个元素地址
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d,", *a);
+//		a++;
+//		printf("%p\n", a);
+//	}
+//}
+
+//int main() {
+//	int arr[10] = { 0 };
+//	int* a = arr;//获取第一个元素地址
+//	//printf("%p", a);
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d", *(a + i));
+//		*(a+i) = 1;
+//		//printf("%p\n", &a + i);
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d,", arr[i]);
+//	}
+//}
+
+///*指针的运算*/
+//int main() {
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d", &arr[0] - &arr[9]);//指针减去指针得出中间的元素个数
+//	return 0;
+//}
+
+/*指针循环*/
+//#define N_VALUES 5
+//int main() {
+//	int a = 20;
+//	int* b = &a;
+//	printf("%p\n", b);
+//	printf("%p", &a);
+//	int values[N_VALUES];
+//	int *vp;
+//	for (vp = &values[0]; vp < &values[N_VALUES];)
+//	{		
+//		*vp++ = 0;
+//		printf("%p\n", vp);
+//	}
+//}
+
+/*数组指针*/
+//int main() {
+//	int arr[10] = { 0 };
+//	printf("%p\n", arr);//直接打印数组地址是,是获取数组第一个元素的位置
+//	printf("%p\n", arr+1);//加一后得到的是第二个数组元素的地址
+//
+//	printf("%p\n", &arr[0]);//取第一个元素的位置
+//	printf("%p\n", &arr[0]+1);//加一后得到的是第二个数组元素的地址
+//
+//	printf("%p\n", &arr);//整个数组元素的地址
+//	printf("%p\n", &arr+1);//跳过整个元素的地址,一个跳过40个字节
+//	//&arr,&数组名-数组名不是首元素地址,而是整个数组的地址;
+//	//sizeof(arr),sizeof(数组名),取出的是整个数组的大小,单位为字节;
+//	return 0;
+//}
+
+/*访问指针数组*/
+//int main() {
+//	int arr[10] = { 0 };
+//	int* p = arr;//p是数组首元素的地址
+//	*p = 10;
+//	*(p + 1) = 20;
+//	printf("%d\n", *(p + 1));
+//	printf("%d\n", *p);
+//	return 0;
+//}
+
+/*二级指针*/
+//int main() {
+//	int a = 20;
+//	int* pa = &a;//一级指针
+//	int** ppa = &pa;//二级指针
+//}
+
+/*指针数组*/
+int main() {
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int* pa = &a;
+	int* pb = &b;
+	int* pc = &c;
+	//如何把整形的指针存放到数组中呢
+	int* arr[3] = { pa,pb,pc };
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%p\n", arr[i]);
+	}
+	return 0;
 }
+
 
